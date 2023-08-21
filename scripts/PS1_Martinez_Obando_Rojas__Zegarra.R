2c227d47f538d3ad3dccd437996df7834310ccf0
David Javier Zegarra Flórez <d.zegarra@uniandes.edu.co>
2023-08-20 22:50:00 -0500

    update script

--- scripts/PS1_Martinez_Obando_Rojas__Zegarra.R
+++ scripts/PS1_Martinez_Obando_Rojas__Zegarra.R
@@ -4,11 +4,23 @@
 # Materia: Big Data and Machine Learning para Economía Aplicada
 # Año: 2023
 ###############################################################
-# Set directory
- 
+# Fijo directorio e instalo paquetes
+
+
+local_dav <- "D:/github/"
+setwd(paste(local_dav,"/PS_1_ML/scripts",sep=""))
+getwd()
+
+#install.packages(c("RSelenium","wdman" "tidyverse"))
+library(tidyverse)
+library(RSelenium)
+library(wdman)
 
 #-------------------------------------------------------------
 # 1. Data
+
+link <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/"
+
 
 #-------------------------------------------------------------
 # 2. Age-wage profile

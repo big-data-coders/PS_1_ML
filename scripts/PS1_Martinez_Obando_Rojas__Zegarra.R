b1f96aaf988d394300faff293f6459b9248c114f
David Javier Zegarra Flórez <d.zegarra@uniandes.edu.co>
2023-09-02 15:26:27 -0500

    update script1

--- scripts/PS1_Martinez_Obando_Rojas__Zegarra.R
+++ scripts/PS1_Martinez_Obando_Rojas__Zegarra.R
@@ -11,24 +11,23 @@ local_dav <- "D:/github/"
 setwd(paste(local_dav,"/PS_1_ML/scripts",sep=""))
 getwd()
 
-#install.packages(c("RSelenium","wdman" "tidyverse"))
-library(tidyverse)
-library(RSelenium)
-library(wdman)
+#**********************************************************
+install.packages("rvest")
+library(pacman)
+library(rvest)
 
 #-------------------------------------------------------------
 # 1. Data
+rm(list=ls())
 
-link <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/"
+link <- paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_", 1:10, ".html")
+link
 
+df <- data.frame()
+for (url in link) {
+  print(url)
+  table0 <- read_html(url) %>%  html_table() 
+  table0 <- as.data.frame(table0[[1]])
+  df <- rbind(df, table0)
+}
 
-#-------------------------------------------------------------
-# 2. Age-wage profile
-
-#-------------------------------------------------------------
-# 3. The gender earnings GAP
-
-#-------------------------------------------------------------
-# 4. Predicting earnings
-
-#-------------------------------------------------------------

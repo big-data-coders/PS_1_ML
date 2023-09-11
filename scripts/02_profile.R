a3cc42fa9c7d42d0796ced1680aa80131b8f9931
kev-meca <ka.martinezm12@uniandes.edu.co>
2023-09-10 21:52:53 -0500

    Update 02_profile.R

---
+++ scripts/02_profile.R
@@ -0,0 +1,32 @@
+
+# 1| Web scrapping --------------------------------------------------------
+# De la página de Ignacio se extrae las diez tablas y se concatenan por filas. 
+install.packages("rvest")
+library(rvest)
+
+if (primeraVez <- TRUE) {
+  link <- paste0(
+    "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_", 1:10, ".html"
+  )
+  
+  dataset <- data.frame()
+  for (url in link) {
+    print(url)
+    table0 <- read_html(url) %>%  html_table() 
+    table0 <- as.data.frame(table0[[1]])
+    dataset <- rbind(dataset, table0)
+  }
+  
+  write.csv(x = dataset, file = paste0(Directoriodatos, 'dataframe.csv'), 
+            row.names = FALSE)
+} else {
+  dataset <- read.csv(file = paste0(Directoriodatos, 'dataframe.csv'))
+}
+
+
+
+
+
+
+
+

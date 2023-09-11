1fbb24afc61f177a05a4cee76409673f1c7904b4
Juan Camilo Obando <jc.obando10@uniandes.edu.co>
2023-09-11 17:52:07 -0500

    Añadí regresiones
    Reu con Juli y David

--- scripts/03_gap.R
+++ scripts/03_gap.R
@@ -16,3 +16,37 @@ if (current_user == "maria") {
 }
 
 data <- read.csv(paste0(path,"/PS_1_ML/stores/dataframe.csv", sep=""))
+glimpse(data)
+data <- data %>% select(y_ingLab_m_ha, sex, relab, age)
+
+
+#Tabla con estadísticas descriptivas
+stargazer(data.frame(data), header=FALSE, type='text',title="Variables Included in the Selected Data Set")
+
+
+#a) 
+reg1<-lm(log(y_ingLab_m_ha) ~ sex, data = data)
+stargazer(reg1,type="text",digits=7)
+
+#b) #falta hacer dummyes de relab
+reg2<-lm(log(y_ingLab_m_ha) ~ sex + relab, data = data)
+stargazer(reg2,type="text",digits=7)
+
+lax <- "relab + age" #como hago para llamarla a continuacion
+
+#FWL: 
+data<-data %>% 
+  drop_na(sex, relab, age) %>% 
+  mutate(sexResid=lm(sex~relab+age,data)$residuals) #Residuals of sex~lax 
+
+
+data<-data %>% mutate(logyResid=lm(logy_ingLab_m_ha~relab+age,data)$residuals) #Residuals of logy~lax 
+
+reg3<-lm(logyResid~sexResid,data)
+stargazer(reg1,reg3,type="text",digits=7) 
+
+
+
+
+
+

1ecbf53901123c5aedf6c3c0f934891896a970df
Juan Camilo Obando <jc.obando10@uniandes.edu.co>
2023-09-16 18:09:10 -0500

    latex a,b
    Agrego todas las variables y exporto a latex las tablas

--- scripts/03_gap.R
+++ scripts/03_gap.R
@@ -3,45 +3,27 @@ require("pacman")
 library(fastDummies)
 p_load("tidyverse","stargazer")
 
-#Lectura de los datos
-current_user <- Sys.info()["user"]
-
-if (current_user == "maria") {
-  path <- "C:/Users/maria/Desktop/git"
-} else if (current_user == "usuario_2") {
-  path <- "ruta/en/otro/computador.csv"
-} else if (current_user == "usuario_3") {
-  path <- "ruta/en/otro/computador.csv"
-} else {
-  path <- "ruta/en/otro/computador.csv"
-}
-
-data <- read.csv(paste0(path,"/PS_1_ML/stores/dataframe.csv", sep=""))
-glimpse(data)
-
-#Limpieza de datos
-data <- data %>% select(y_ingLab_m_ha, sex, relab, age)
-data<-data %>% drop_na(y_ingLab_m_ha, sex, relab, age)
-data <- dummy_cols(data, select_columns = "relab") #Creo las dummys para esta categoría
-data$relab <- NULL
-glimpse(data)
+glimpse(dataset)
 
 #Tabla con estadísticas descriptivas
-stargazer(data.frame(data), header=FALSE, type='text',title="Variables Included in the Selected Data Set")
+stargazer(data.frame(dataset), header=FALSE, type='text',title="Variables Included in the Selected Data Set")
 
 #a) 
-reg1<-lm(log(y_ingLab_m_ha) ~ sex, data = data)
-stargazer(reg1,type="text",digits=7)
+reg1<-lm(log(num_salarioHora) ~ cat_sexo, data = dataset)
+stargazer(reg1, type="text", digits=7) 
+stargazer(reg1, digits=7, align=TRUE, type="latex", out="reg1")
 
 #b) 
-reg2<-lm(log(y_ingLab_m_ha) ~ relab_1+relab_2+relab_3+relab_8+age, data = data)
+reg2<-lm(log(num_salarioHora) ~ cat_sexo+id_vivienda+id_hogar+id_persona+cat_posicion+cat_ocupacion+cat_estrato+cat_formalidad+cat_empresa+cat_educacion+num_edad, data = dataset)
 stargazer(reg2,type="text",digits=7)
+stargazer(reg2, digits=7, align=TRUE, type="latex", out="reg2")
 
 #FWL: 
-data<-data %>% mutate(sexResid=lm(sex~relab_1+relab_2+relab_3+relab_8+age,data)$residuals) #Residuals of sex~x 
-data<-data %>% mutate(logyResid=lm(log(y_ingLab_m_ha)~relab_1+relab_2+relab_3+relab_8+age,data)$residuals) #Residuals of logy~x 
+dataset<-dataset %>% mutate(sexResid=lm(cat_sexo~id_vivienda+id_hogar+id_persona+cat_posicion+cat_ocupacion+cat_estrato+cat_formalidad+cat_empresa+cat_educacion+num_edad,dataset)$residuals) #Residuals of sex~x 
+dataset<-dataset %>% mutate(logyResid=lm(log(num_salarioHora)~id_vivienda+id_hogar+id_persona+cat_posicion+cat_ocupacion+cat_estrato+cat_formalidad+cat_empresa+cat_educacion+num_edad,dataset)$residuals) #Residuals of logy~x 
 reg3<-lm(logyResid~sexResid,data)
 stargazer(reg1,reg3,type="text",digits=7) 
+stargazer(reg2, digits=7, align=TRUE, type="latex", out="reg2")
 
 #Boostrap
 p_load("boot")
